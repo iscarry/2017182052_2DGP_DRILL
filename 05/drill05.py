92a10e13b12e119e339d4945e5ce8c6c8c12f8f8
iscarry <00tyty003@gmail.com>
2022-09-19 15:58:48 +0900

    DRIIL 05 완성
    2017182052 최무성

---
+++ 05/drill05.py
@@ -0,0 +1,34 @@
+import turtle
+
+def turtle_move_Up():
+    turtle.setheading(90)
+    turtle.stamp()
+    turtle.forward(30)
+
+def turtle_move_Down():
+    turtle.setheading(270)
+    turtle.stamp()
+    turtle.forward(30)
+
+def turtle_move_Left():
+    turtle.setheading(180)
+    turtle.stamp()
+    turtle.forward(30)
+
+def turtle_move_Right():
+    turtle.setheading(0)
+    turtle.stamp()
+    turtle.forward(30)
+
+def escape():
+    turtle.reset()
+
+
+turtle.shape("turtle")
+turtle.onkey(escape,'Escape')
+turtle.onkey(turtle_move_Up,'Up')
+turtle.onkey(turtle_move_Down,'Down')
+turtle.onkey(turtle_move_Left,'Left')
+turtle.onkey(turtle_move_Right,'Right')
+
+turtle.listen()
