6be69d2ea4e10d7a7d591780c20c6e7dda352478
iscarry <00tyty003@gmail.com>
2022-09-26 22:51:57 +0900

    DRILL07 완성
    2017182052
    최무성

--- 07/anim_viewer.py
+++ 07/anim_viewer.py
@@ -1,13 +1,13 @@
 from pico2d import *
 
 open_canvas(900, 640)
+grass = load_image('grass.png')
 character = load_image('pngwing01.com.png')
 character02 = load_image('pngwing.com02.png')
+character03 = load_image('pngwing.com03.png')
+character04 = load_image('pngwing.com04.png')
 
 running = True
-x = 50
-frame = 0
-frame2 = 0
 
 def handle_events():
     global running
@@ -18,18 +18,58 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
-
 while running:
-    clear_canvas()
-    character.clip_draw(frame * 90, 0, 80, 100, 150, 300)
-    frame = (frame + 1) % 6
-    character02.clip_draw(frame * 90, 0, 80, 100, 300, 300)
-    frame2 = (frame2 + 1) % 7
+    frame = 0
+    frame2 = 0
+    frame3 = 0
+    frame4 = 0
+    x = 500
 
-    update_canvas()
+    while x > 450 and running:
+        clear_canvas()
+        grass.draw(450, 30)
+        character03.clip_draw(frame3 * 90, 0, 80, 90, x, 90)
+        update_canvas()
+
+        handle_events()
+        frame3 = (frame3 + 1) % 4
+        x -= 5
+        delay(0.1)
+
+    while running:
+        clear_canvas()
+        grass.draw(450, 30)
+        character.clip_draw(frame * 90, 0, 80, 100, 450, 90)
+        if frame == 6:
+            break
+        frame = (frame + 1) % 7
+
+        update_canvas()
+        delay(0.1)
+
+    while running:
+        clear_canvas()
+        grass.draw(450, 30)
+        character02.clip_draw(frame2 * 90, 0, 80, 100, 450, 90)
+        if frame2 == 7:
+            break
+        frame2 = (frame2 + 1) % 8
+
+        update_canvas()
+        delay(0.1)
+
+    while running:
+        clear_canvas()
+        grass.draw(450, 30)
+        character04.clip_draw(frame4 * 90, 0, 80, 110, 450, 90)
+        if frame4 == 7:
+            break
+        frame4 = (frame4 + 1) % 8
+
+        update_canvas()
+        delay(0.1)
+
     handle_events()
-
-    delay(0.1)
 
 close_canvas()
 
