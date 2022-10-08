55120217ba2a30dd55d8e80684fcbe1b55b95a90
iscarry <00tyty003@gmail.com>
2022-10-08 14:20:44 +0900

    sprit animation battleship
    battleship sprite animaition

--- 2DGP_SpaceGuardian/main.py
+++ 2DGP_SpaceGuardian/main.py
@@ -16,27 +16,110 @@ RED = (250, 50, 50)
 FPS = 60
 
 #클레스
+
 class BattleShip(pygame.sprite.Sprite):
     def __init__(self):
         super(BattleShip, self).__init__()
-        self.image = pygame.image.load('Battleship.png')
+        self.images = []
+        # 0 가만히 있는 상태
+        self.images.append(pygame.image.load('Battleship.png'))
+        # 1 ~ 2 오른쪽 움직임
+        self.images.append(pygame.image.load('Battleship_R1.png'))
+        self.images.append(pygame.image.load('Battleship_R2.png'))
+        # 3 ~ 4 왼쪽 움직임
+        self.images.append(pygame.image.load('Battleship_L1.png'))
+        self.images.append(pygame.image.load('Battleship_L2.png'))
+        # 5 ~ 6 위쪽 움직임
+        self.images.append(pygame.image.load('Battleship_U1.png'))
+        self.images.append(pygame.image.load('Battleship_U2.png'))
+        # 7 ~ 8 아래쪽 움직임
+        self.images.append(pygame.image.load('Battleship_D1.png'))
+        self.images.append(pygame.image.load('Battleship_D2.png'))
+
+        self.images_stay = self.images
+        self.images_right = self.images
+        self.images_left = self.images
+        self.images_up = self.images
+        self.images_down = self.images
+        self.state = 0
+        self.direction = 'stay'
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.index = 0
+        self.image = self.images[self.index]
         self.rect = self.image.get_rect()
+        self.frame = round(100 / len(self.images*100), 2)
+        self.current_time = 0
+        self.clock = pygame.time.Clock()
         self.reset()
 
     def reset(self):
         self.rect.x = (SCREEN_WIDTH * 0.4)
         self.rect.y = (SCREEN_HEIGHT * 0.8)
-        self.dx = 0
-        self.dy = 0
     def update(self):
-        self.rect.x += self.dx
-        self.rect.y += self.dy
 
+        if self.state == 0:
+            count = 1
+            start_index = 0
+            self.velocity_x = 0
+            self.velocity_y = 0
+        elif self.state == 1:
+            count = 2
+            start_index = 1
+            self.velocity_x = 5
+            self.velocity_y = 0
+        elif self.state == 2:
+            count = 2
+            start_index = 3
+            self.velocity_x = 5
+            self.velocity_y = 0
+
+        elif self.state == 3:
+            count = 2
+            start_index = 5
+            self.velocity_x = 0
+            self.velocity_y = 5
+
+        elif self.state == 4:
+            count = 2
+            start_index = 7
+            self.velocity_x = 0
+            self.velocity_y = 5
+
+        if self.direction == 'stay':
+            self.images = self.images_stay
+        elif self.direction == 'right':
+            self.images = self.images_right
+        elif self.direction == 'left':
+            self.images = self.images_left
+            self.velocity_x = abs(self.velocity_x) * -1
+        elif self.direction == 'up':
+            self.images = self.images_up
+            self.velocity_y = abs(self.velocity_y) * -1
+        elif self.direction == 'down':
+            self.images = self.images_down
+
+        self.current_time += self.clock.tick(FPS)
+
+        if self.current_time >= self.frame:
+            self.current_time = 0
+
+            self.index = (self.index % count) + start_index
+            self.image = self.images[self.index]
+            self.index += 1
+
+            if self.index >= len(self.images):
+                self.index = 0
+
+        self.rect.x += self.velocity_x
+        self.rect.y += self.velocity_y
+
+        # 스크린에서 나가지 않게 해주는 코드
         if self.rect.x < 0 or self.rect.x + self.rect.width > SCREEN_WIDTH:
-            self.rect.x -= self.dx
+            self.rect.x -= self.velocity_x
 
         if self.rect.y < 0 or self.rect.y + self.rect.height > SCREEN_HEIGHT:
-            self.rect.y -= self.dy
+            self.rect.y -= self.velocity_y
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
@@ -112,13 +195,17 @@ class Game():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    self.battleship.dx -= 5
+                    self.battleship.direction = "left"
+                    self.battleship.state = 2
                 elif event.key == pygame.K_RIGHT:
-                    self.battleship.dx += 5
+                    self.battleship.direction = "right"
+                    self.battleship.state = 1
                 elif event.key == pygame.K_UP:
-                    self.battleship.dy -= 5
+                    self.battleship.direction = "up"
+                    self.battleship.state = 3
                 elif event.key == pygame.K_DOWN:
-                    self.battleship.dy += 5
+                    self.battleship.direction = "down"
+                    self.battleship.state = 4
                 elif event.key == pygame.K_SPACE:
                     fire = Fire(self.battleship.rect.centerx, self.battleship.rect.y, 10)
                     self.fires.add(fire)
@@ -127,9 +214,11 @@ class Game():
 
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-                    self.battleship.dx = 0
+                    self.battleship.direction = "stay"
+                    self.battleship.state = 0
                 elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    self.battleship.dy = 0
+                    self.battleship.direction = "stay"
+                    self.battleship.state = 0
 
 
         return False
