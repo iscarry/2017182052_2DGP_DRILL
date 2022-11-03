3173d57ff6d3ea79ab8d55605f46184614d0e281
iscarry <00tyty003@gmail.com>
2022-11-03 17:37:34 +0900

    Drill12 완성
    2017182052 최무성

---
+++ 12/ball.py
@@ -0,0 +1,21 @@
+from pico2d import*
+import game_world
+#from boy import Boy
+
+class Ball:
+    image = None
+    #boy = Boy()
+    def __init__(self, x=800, y=300, velocity=1):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x, self.y, self.velocity = x, y, velocity
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        self.x += self.velocity
+
+        if self.x < 25 or self.x > 800 - 25:
+            game_world.remove_object(self)
+
